1c6638a1a998297763e063a3bda73e0b5aa1e480
Muntasir-Mahmud <m.siyam529@gmail.com>
2024-03-15 01:01:22 +0600

    Backend: setup a basic data pipeline for the llm response

--- backend/main.py
+++ backend/main.py
@@ -2,35 +2,31 @@ from fastapi import FastAPI
 from models.chatbot_query import ChatbotQueryInput, ChatbotQueryOutput
 from utils.async_utils import async_retry
 
+from chains.chatbot_query_chain import chatbot_executor
+
 app = FastAPI(
     title="Customer Support Chatbot",
     description="Endpoints for a customer support chatbot",
 )
 
 
-@async_retry(max_retries=10, delay=1)
-async def invoke_llm_with_retry(query: str):
+def invoke_llm_with_retry(query: str):
     """
     Retry the agent if a tool fails to run. This can help when there
     are intermittent connection issues to external APIs.
     """
-    pass
-    # return await hospital_rag_agent_executor.ainvoke({"input": query})
-
+    return chatbot_executor(query)
 
 
 @app.get("/")
-async def get_status():
+def get_status():
     return {"status": "running"}
 
 
 @app.post("/chatbot")
-async def query_chatbot(
-    query: ChatbotQueryInput,
+def query_chatbot(
+        query: ChatbotQueryInput,
 ) -> ChatbotQueryOutput:
-    query_response = await invoke_agent_with_retry(query.text)
-    query_response["intermediate_steps"] = [
-        str(s) for s in query_response["intermediate_steps"]
-    ]
+    query_response = invoke_llm_with_retry(query.text)
 
     return query_response
